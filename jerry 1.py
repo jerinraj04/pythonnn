d8db4cc66c268b36d383fd2e4f41aba3bd4f9463
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2018-09-20 11:32:19 +0530

    Update jerry 1.py

--- jerry 1.py
+++ jerry 1.py
@@ -1,7 +1,7 @@
 if num > 1:
  for i in range(2,num):
        if (num % i) == 0:
-           print(num,"is not a prime number")
+         print(num,"is not a prime number")
            print(i,"times",num//i,"is",num)
            break
    else:
