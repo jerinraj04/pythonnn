3b53ff09c280b2fa396e1d45d555ecc6bd450e08
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2018-09-20 12:17:48 +0530

    Update jerry8.py

--- jerry8.py
+++ jerry8.py
@@ -1,12 +1,6 @@
 def multiple(m, n): 
-  
-    # inserts all elements from n to  
-    # (m * n)+1 incremented by n. 
-    a = range(n, (m * n)+1, n) 
-      
-    print(*a) 
-  
-# driver code  
+a = range(n, (m * n)+1, n) 
+print(*a)   
 m = 4
 n = 3
 multiple(m, n) 
