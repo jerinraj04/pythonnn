3077415e4af9615ed4d0e7da5cb886562cb5ef93
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2018-09-20 11:57:06 +0530

    Update jerry4.py

--- jerry4.py
+++ jerry4.py
@@ -1,7 +1,6 @@
-print("Prime numbers between",lower,"and",upper,"are:")
+print("prime numbers between",lower,"and",upper,"are:")
 
 for num in range(lower,upper + 1):
-   # prime numbers are greater than 1
    if num > 1:
        for i in range(2,num):
            if (num % i) == 0:
