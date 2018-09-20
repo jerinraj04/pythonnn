f42c79c01d19662d875f1bf921ea8f68383e7214
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2018-09-20 12:13:23 +0530

    Update jerry7.py

--- jerry7.py
+++ jerry7.py
@@ -1,6 +1,6 @@
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
+if num < o:
+   print("Sorry actorial does not exist for negative numbers")
+   else if num==0;
    print("The factorial of 0 is 1")
 else:
    for i in range(1,num + 1):
