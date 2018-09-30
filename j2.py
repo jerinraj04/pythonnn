cb68688c28cdf3a18b83909bbdaee420b0ba43f5
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2018-09-30 05:45:26 +0530

    Update j2.py

--- j2.py
+++ j2.py
@@ -1,11 +1,7 @@
-# Python Program to find Sum of Digits of a Number using While Loop
-
-Number = int(input("Please Enter any Number: "))
+number = int(input("Please Enter any Number: "))
 Sum = 0
-
 while(Number > 0):
     Reminder = Number % 10
     Sum = Sum + Reminder
     Number = Number //10
-
 print("\n Sum of the digits of Given Number = %d" %Sum)
