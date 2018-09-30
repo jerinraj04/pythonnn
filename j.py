35b7a968714381a49dfd465c1d1ba27edb93fd4e
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2018-09-30 05:34:16 +0530

    Update j.py

--- j.py
+++ j.py
@@ -1,37 +1,16 @@
-# Python3 program to print the number which 
-# contain the digit d from 0 to n 
-  
-# Returns true if d is present as digit 
-# in number x. 
 def isDigitPresent(x, d): 
-  
-    # Breal loop if d is present as digit 
-    while (x > 0): 
-      
-        if (x % 10 == d): 
+  while (x > 0): 
+      if (x % 10 == d): 
             break
-  
-        x = x / 10
-      
-  
-    # If loop broke 
-    return (x > 0) 
-  
-  
-# function to display the values 
-def printNumbers(n, d): 
-  
-    # Check all numbers one by one 
+ x = x / 10
+      return (x > 0) 
+  def printNumbers(n, d): 
+
     for i in range(0, n+1): 
-  
-        # checking for digit 
-        if (i == d or isDigitPresent(i, d)): 
+ if (i == d or isDigitPresent(i, d)): 
             print(i,end=" ") 
-  
-# Driver code 
-n = 47
+  n = 47
 d = 7
 print("The number of values are") 
 printNumbers(n, d) 
-#This code is contributed by 
-#Smitha Dinesh Semwal 
+
